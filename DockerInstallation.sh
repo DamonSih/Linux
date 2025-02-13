a5a7cd6b4dd310b5a9063e54f75351bd5d47bc50
themazdarati <165470773+themazdarati@users.noreply.github.com>
2025-02-14 00:14:42 +0800

    Create DockerInstallation.sh

---
+++ DockerInstallation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+echo "Docker has been installed. The current user is added to Docker group."
