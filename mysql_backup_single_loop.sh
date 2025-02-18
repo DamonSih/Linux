408e006289f7f537f60bd7ac6ad85d764fb0c665
Damon Sih <165470773+DamonSih@users.noreply.github.com>
2025-02-18 23:48:47 +0800

    Update and rename mysql_backup.sh to mysql_backup_single_loop.sh

---
+++ mysql_backup_single_loop.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+USER="your_mysql_user"
+PWD="your_mysql_password"
+DB_NAME="your_database_name"
+BACKUP_DIR="/path/to/backup"
+DATE=$(date +"%Y-%m-%d")
+
+# Get list of databases, excluding system databases
+DB_LIST=$(mysql -u $USER -p $PWD -s -e "show databases;" 2>/dev/null | egrep -v "Database|information_schema|mysql|performance_schema|sys")
+
+# Loop through each database and back it up
+for DB in $DB_LIST; do
+    BACKUP_NAME="${BACKUP_DIR}/${DB}_${DATE}.sql"
+    
+    if ! mysqldump -h$HOST -u$USER -p$PWD -B $DB > "$BACKUP_NAME" 2>/dev/null; then
+        echo "$BACKUP_NAME backup is unsuccessful!"
+    else
+        echo "$BACKUP_NAME backup is successful!"
+    fi
+done
